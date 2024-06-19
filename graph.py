832c06f2bd9206b34388eee813b95ec865b01614
rorschach3 <daniel_fhernandez@yahoo.com>
2024-06-19 16:58:46 -0700

    added more graphs

--- graph.py
+++ graph.py
@@ -4,28 +4,51 @@ import yfinance as yf
 
 
 def graph_stock_data(tickers, start_date, end_date, interval='1d'):
-    start_date = "2023-01-1"
-    end_date = "2023-12-31"
-
-    tickers = ["DAL", "AAL"]
-
-    combined = pd.DataFrame()
+    def download_stock_data(tickers):
+        return yf.download(tickers, start=start_date, end=end_date, interval=interval)['Adj Close']
 
     try:
-        for ticker in tickers:
-            data = yf.download(ticker, start=start_date, end=end_date)
-            combined[ticker] = data[['Adj Close']]
+        combined = pd.DataFrame({ticker: download_stock_data(ticker) for ticker in tickers})
 
-        for ticker in tickers:
-            plt.plot(combined[ticker], label=ticker)
+        plt.figure()
+        for col in combined.columns:
+            plt.plot(combined.index, combined[col], label=f"{col} Adj Closing Prices")
 
-        plt.legend(loc="upper right")
-        plt.title(tickers[0] + " vs " + tickers[1])
-        plt.ylabel("closing prices")
-        plt.xlabel("Years")
+        plt.legend(loc="upper left")
+        plt.title("Adjusted Closing Prices Comparison")
+        plt.ylabel("Closing Prices")
+        plt.xlabel("Date")
+        plt.grid(True)
         plt.show()
 
+        if len(tickers) == 2:
+            pct_change = combined.pct_change().dropna()
+
+            plt.figure()
+            for col in pct_change.columns:
+                plt.plot(pct_change.index, pct_change[col], label=f"{col} Percent Change")
+
+            plt.legend(loc="upper left")
+            plt.title("Percent Change Comparison ")
+            plt.ylabel("Percent Change")
+            plt.xlabel("Date")
+            plt.grid(True)
+            plt.show()
+
+            cum_returns = (1 + pct_change).cumprod() - 1
+
+            plt.figure()
+            for col in cum_returns.columns:
+                plt.plot(cum_returns.index, cum_returns[col], label=f"{col} Cumulative Returns")
+
+            plt.legend(loc="upper left")
+            plt.title("Cumulative Returns Comparison")
+            plt.ylabel("Cumulative Returns")
+            plt.xlabel("Date")
+            plt.grid(True)
+            plt.show()
+
     except Exception as e:
-        print(f"Error occured: {e}")
+        print(f"Error occurred: {e}")
 
     print(combined)
--- stock_data.py
+++ stock_data.py
@@ -1,27 +1,31 @@
 import yfinance as yf
 import numpy as np
 import statsmodels.api as sm
-import pandas as pd
+# import pandas as pd
+from graph import graph_stock_data
 
 
 # Function to Compute Range
 def compute_range(nums):
-    return np.max(nums) - np.min(nums)
+    return np.ptp(nums)
 
 
 # Function to compute Interquartile Range (IQR)
-def compute_iqr(nums):  # To Calculate IQR it is Q3 - Q1
-    q75, q25 = np.percentile(nums, [75, 25])
-    return q75 - q25
+def compute_iqr(nums):
+    return np.percentile(nums, 75) - np.percentile(nums, 25)
 
 
 # Function to compute correlation
 def compute_correlation(data1, data2):
-    correlation = np.corrcoef(data1['Close'], data2['Close'])[0, 1]
-    return correlation
+    return data1["Close"].corr(data2["Close"])
 
 
 # Function to compute cointegration
+    """ This uses the augmented Engle-Granger two-step cointegration test.
+    Constant or trend is included in 1st stage regression, i.e. in
+    cointegrating equation. """
+
+
 def compute_cointegration(data1, data2):
     cointegration_result = sm.tsa.stattools.coint(data1['Close'], data2['Close'])
     return cointegration_result
@@ -39,47 +43,34 @@ def fetch_stock_data(ticker, start_date, end_date, interval='1d'):
 
 # Analyze stock data
 def analyze_stock_data(data, ticker):
+
+    # Compute range and IQR
+    range_val = compute_range(data["Close"])
+    iqr_val = compute_iqr(data["Close"])
+
     print(f"\n\nAnalyzing data for {ticker}:")
     print("------------------------------------------------")
-
-    print(f"{ticker} Mean Price: {abs(data['Close'].mean()):.2f}")
-    print(f"{ticker} Variance Price: {abs(data['Close'].var()):.2f}")
-    print(f"{ticker} Maximum Price: {abs(data['Close'].max()):.2f}")
-    print(f"{ticker} Minimum Price: {abs(data['Close'].min()):.2f}")
-    print(f"{ticker} Standard Deviation: {abs(data['Close'].std()):.2f}\n")
-
-
-# Function definitions for statistical calculations
-def compute_maximum(nums):
-    return nums.max()
-
-
-def compute_minimum(nums):
-    return nums.min()
-
-
-def compute_mean(nums):
-    return nums.mean()
-
-
-def compute_variance(nums):
-    return nums.var()
-
-
-def compute_std_dev(nums):
-    return nums.std()
+    print(f"{ticker} Mean Price: {data['Close'].mean():.6f}")
+    print(f"{ticker} Maximum Price: {data['Close'].max():.6f}")
+    print(f"{ticker} Minimum Price: {data['Close'].min():.6f}\n")
+    print("*** Spread ***")
+    print(f"{ticker} Variance Price: {data['Close'].var():.6f}")
+    print(f"{ticker} Range: {range_val:.6f}")
+    print(f"{ticker} Standard Deviation: {data['Close'].std():.6f}")
+    print(f"{ticker} Interquartile Range (IQR): {iqr_val:.6f}\n")
 
 
 # Main function to fetch and analyze data
 def main():
+    tickers = ["DAL", "AAL"]
     ticker1 = "DAL"  # Delta Airlines
     ticker2 = "AAL"  # American Airlines
-    start_date = "2022-12-31"
+    start_date = "2022-12-30"
     end_date = "2023-12-30"
 
     # Fetch stock data
-    data1 = fetch_stock_data(ticker1, start_date, end_date)
-    data2 = fetch_stock_data(ticker2, start_date, end_date)
+    data1 = fetch_stock_data(ticker1, start_date, end_date, interval='1d')
+    data2 = fetch_stock_data(ticker2, start_date, end_date, interval='1d')
 
     # Analyze the stock data for each ticker
     analyze_stock_data(data1, ticker1)
@@ -88,31 +79,19 @@ def main():
     # Compute cointegration with other ticker
     cointegration = compute_cointegration(data1, data2)
 
-    # Compute range and IQR
-    close_prices_DAL = data1["Close"]
-    range_val1 = compute_range(close_prices_DAL)
-    iqr_val1 = compute_iqr(close_prices_DAL)
-    close_prices_AAL = data2["Close"]
-    range_val2 = compute_range(close_prices_AAL)
-    iqr_val2 = compute_iqr(close_prices_AAL)
-
     # Compare the differences between the two stocks
     print("\nComparing the differences between DAL and AAL:")
     print("------------------------------------------------")
-    print(f"{ticker1} Range: {range_val1:.6f}")
-    print(f"{ticker2} Range: {range_val2:.2f}")
-    print(f"{ticker1} Interquartile Range (IQR): {iqr_val1:.6f}")
-    print(f"{ticker2} Interquartile Range (IQR): {iqr_val2:.6f}")
     print(f"Cointegration p-value: {cointegration[1]:.6f}")
     correlation = compute_correlation(data1, data2)
     print(f"Correlation: {correlation:.6f}")
-    print(
-        f"Standard Deviation Difference: {abs(data1['Close'].std() - data2['Close'].std()):.6f}\n"
-    )
+    std_diff = abs(data1["Close"].std() - data2["Close"].std())
+    print(f"Standard Deviation Difference: {std_diff:.6f}\n")
 
     # Graph the stock data, Un comment below to include graph data
-    # graph_data = graph_stock_data(ticker, start_date, end_date)
-    # return (graph_data)
+    graph_data = graph_stock_data(tickers, start_date, end_date)
+    for ticker in tickers:
+        return (graph_data)
 
 
 if __name__ == "__main__":
