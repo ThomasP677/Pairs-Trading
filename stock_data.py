b40d1a6f6751dfae4cbac871dcc57385c1a89727
rorschach3 <daniel_fhernandez@yahoo.com>
2024-06-18 21:32:34 -0700

    Refactor stock_data.py for improved readability and maintainability

--- stock_data.py
+++ stock_data.py
@@ -1,7 +1,7 @@
 import yfinance as yf
-from graph import graph_stock_data
 import numpy as np
 import statsmodels.api as sm
+import pandas as pd
 
 
 # Function to Compute Range
@@ -10,7 +10,7 @@ def compute_range(nums):
 
 
 # Function to compute Interquartile Range (IQR)
-def compute_iqr(nums):
+def compute_iqr(nums):  # To Calculate IQR it is Q3 - Q1
     q75, q25 = np.percentile(nums, [75, 25])
     return q75 - q25
 
@@ -31,14 +31,14 @@ def compute_cointegration(data1, data2):
 def fetch_stock_data(ticker, start_date, end_date, interval='1d'):
     stock = yf.Ticker(ticker).history(start=start_date, end=end_date, interval='1d')
     if not stock.empty:
-        print(f"Data for {ticker} downloaded successfully.")
+        print(f"{ticker} downloaded successfully.")
     else:
-        print(f"Failed to download data for {ticker}.")
+        print(f"Failed to download {ticker}.")
     return stock
 
 
 # Analyze stock data
-def analyze_stock_data(data, ticker,):
+def analyze_stock_data(data, ticker):
     print(f"\n\nAnalyzing data for {ticker}:")
     print("------------------------------------------------")
 
@@ -72,7 +72,6 @@ def compute_std_dev(nums):
 
 # Main function to fetch and analyze data
 def main():
-    ticker = ["DAL", "AAL"]  # List of tickers to analyze
     ticker1 = "DAL"  # Delta Airlines
     ticker2 = "AAL"  # American Airlines
     start_date = "2022-12-31"
@@ -93,7 +92,6 @@ def main():
     close_prices_DAL = data1["Close"]
     range_val1 = compute_range(close_prices_DAL)
     iqr_val1 = compute_iqr(close_prices_DAL)
-
     close_prices_AAL = data2["Close"]
     range_val2 = compute_range(close_prices_AAL)
     iqr_val2 = compute_iqr(close_prices_AAL)
@@ -101,30 +99,20 @@ def main():
     # Compare the differences between the two stocks
     print("\nComparing the differences between DAL and AAL:")
     print("------------------------------------------------")
-    print(f"{ticker1} Range: {range_val1:.2f}")
+    print(f"{ticker1} Range: {range_val1:.6f}")
     print(f"{ticker2} Range: {range_val2:.2f}")
-    print(f"{ticker1} Interquartile Range (IQR): {iqr_val1:.2f}")
-    print(f"{ticker2} Interquartile Range (IQR): {iqr_val2:.2f}")
+    print(f"{ticker1} Interquartile Range (IQR): {iqr_val1:.6f}")
+    print(f"{ticker2} Interquartile Range (IQR): {iqr_val2:.6f}")
     print(f"Cointegration p-value: {cointegration[1]:.6f}")
+    correlation = compute_correlation(data1, data2)
+    print(f"Correlation: {correlation:.6f}")
     print(
-        f"Mean Price Difference: {abs(data1['Close'].mean() - data2['Close'].mean()):.2f}"
-    )
-    print(
-        f"Variance Price Difference: {abs(data1['Close'].var() - data2['Close'].var()):.2f}"
-    )
-    print(
-        f"Maximum Price Difference: {abs(data1['Close'].max() - data2['Close'].max()):.2f}"
-    )
-    print(
-        f"Minimum Price Difference: {abs(data1['Close'].min() - data2['Close'].min()):.2f}"
-    )
-    print(
-        f"Standard Deviation Difference: {abs(data1['Close'].std() - data2['Close'].std()):.2f}\n"
+        f"Standard Deviation Difference: {abs(data1['Close'].std() - data2['Close'].std()):.6f}\n"
     )
 
-    # Graph the stock data
-    graph_data = graph_stock_data(ticker, start_date, end_date)
-    return (graph_data)
+    # Graph the stock data, Un comment below to include graph data
+    # graph_data = graph_stock_data(ticker, start_date, end_date)
+    # return (graph_data)
 
 
 if __name__ == "__main__":
